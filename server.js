307652c477ea2a37755058e3726d3ecd9fbb98f3
tamils30 <ntamilselvimca@gmail.com>
2016-06-12 21:17:50 +0530

    logincard

--- server.js
+++ server.js
@@ -16,7 +16,54 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false })
 app.get('/', function (req, res) {
    res.sendFile("app/index.html" );
 })
+app.post('/checkschool-card',  urlencodedParser,function (req, res)
+{
+  var id={"id":req.query.username};
 
+       connection.query('SELECT name from md_school where id=(select school_id from employee where ?) ',[id],
+        function(err, rows)
+        {
+    if(!err)
+    {
+    if(rows.length>0)
+    {
+console.log(rows);
+      res.status(200).json({'returnval': rows});
+    }
+    else
+    {
+
+      res.status(200).json({'returnval': 'invalid'});
+    }
+  }
+});
+  });
+
+
+//select the username and password from login table
+app.post('/login-card',  urlencodedParser,function (req, res)
+{
+  var id={"id":req.query.username};
+  var username={"id":req.query.username};
+    var password={"password":req.query.password};
+       connection.query('SELECT role_name,(select school_id from employee where ?) as school,(select name from md_school where id=school) as name ,(select address from md_school where id=school) as addr from role where id=(select role_id from employee where ? and ?) ',[id,username,password],
+        function(err, rows)
+        {
+    if(!err)
+    {
+    if(rows.length>0)
+    {
+
+      res.status(200).json({'returnval': rows});
+    }
+    else
+    {
+
+      res.status(200).json({'returnval': 'invalid'});
+    }
+  }
+});
+  });
 
 app.post('/getroute' ,  urlencodedParser,function (req, res)
 {
